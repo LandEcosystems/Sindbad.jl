2923e574a0860499e7b5a78e8e6607dc0d67007f
dr-ko <skoirala@bgc-jena.mpg.de>
2026-01-15 11:38:40 +0100

    fix unknown option

--- docs/make.jl
+++ docs/make.jl
@@ -88,7 +88,6 @@ if should_deploy
         target = joinpath(@__DIR__, "build"), # this is where Vitepress stores its output
         branch = "gh-pages",
         devbranch = "main",
-        versions = ["stable" => "v^", "v#.#" => "v#.#", "dev" => "dev"],
         push_preview = true
     )
 end
