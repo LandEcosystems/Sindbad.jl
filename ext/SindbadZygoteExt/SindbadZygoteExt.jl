3530e5cf4763a0baa6744ab8964df7c07bd075b3
Lazaro <lazarus.alon@gmail.com>
2026-02-17 14:52:39 +0100

    zygote

---
+++ ext/SindbadZygoteExt/SindbadZygoteExt.jl
@@ -0,0 +1,19 @@
+"""
+    SindbadZygoteExt
+
+Julia extension module that enables Zygote backends for `Sindbad.MachineLearning`.
+
+# Notes:
+- This module is loaded automatically by Julia's package extension mechanism when Zygote is available (see root `Project.toml` `[weakdeps]` + `[extensions]`).
+- End users typically should not `using SindbadZygoteExt` directly; instead `using Sindbad` is sufficient once the weak dependency is installed.
+- The extension code is included in the `ext/` directory and is automatically loaded when the extension package is installed.
+
+Modify the code in the "MachineLearningGradientSite.jl" file to extend the package.
+"""
+module SindbadZygoteExt
+
+    using Zygote
+    
+    include("getPullback.jl")
+
+end
