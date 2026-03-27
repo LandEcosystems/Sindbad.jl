840340621e96671eae98f398dd96d71da718e855
Lazaro Alonso <lazarus.alon@gmail.com>
2026-03-27 09:24:46 +0100

    load data lazily

--- src/DataLoaders/utilsDataLoaders.jl
+++ src/DataLoaders/utilsDataLoaders.jl
@@ -1,5 +1,8 @@
 export getNumberOfTimeSteps
 export mapCleanData
+export cleanData
+export applyUnitConversion
+export applyQCBound
 export subsetAndProcessYax
 export yaxCubeToKeyedArray
 export toDimStackArray
@@ -430,16 +433,19 @@ function subsetAndProcessYax(yax, forcing_mask, tar_dims, _data_info, info, ::Va
     end
 
     #todo mean of the data instead of zero or nan
-    vfill = 0.0
-    if fill_nan
-        vfill = NaN
-    end
-    vNT = Val{num_type}()
-    if clean_data
-        yax = mapCleanData(yax, yax_qc, vfill, bounds_qc, _data_info, vNT)
-    else
-        yax = map(yax_point -> replace_invalid_number(yax_point, vfill), yax)
-        # yax = num_type.(yax)
+    # do a proper check on input type for forcing and if is not lazy do the cleanup
+    if info.experiment.exe_rules.land_output_type == "array"
+        vfill = 0.0
+        if fill_nan
+            vfill = NaN
+        end
+        vNT = Val{num_type}()
+        if clean_data
+            yax = mapCleanData(yax, yax_qc, vfill, bounds_qc, _data_info, vNT)
+        else
+            yax = map(yax_point -> replace_invalid_number(yax_point, vfill), yax)
+            yax = num_type.(yax)
+        end
     end
     return yax
 end
