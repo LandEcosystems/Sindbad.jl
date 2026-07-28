a663a0751ec3b7e0c3a0ca8c20dd2329b1af4b6c
dr-ko <skoirala@bgc-jena.mpg.de>
2026-07-28 09:13:44 +0200

    Update example scripts to use the unified plotTimeSeries/plotPerformanceHistograms API

--- examples/exp_WROASTED/experiment_sequence.jl
+++ examples/exp_WROASTED/experiment_sequence.jl
@@ -1,6 +1,7 @@
 using Revise
 using Sindbad
 using Dates
+using Plots
 
 using NetCDF
 toggle_type_abbrev_in_stacktrace()
@@ -76,51 +77,18 @@ for domain ∈ sites
     observation = out_opti.observation;
 
     # some plots
+    plotTimeSeries(out_opti)
+
     optimized_data = out_opti.output.optimized
     default_data = out_opti.output.default
-    costOpt = prepCostOptions(out_opti.observation, info.optimization.cost_options)
-    default(titlefont=(20, "times"), legendfontsize=18, tickfont=(15, :blue))
-    fig_prefix = joinpath(info.output.dirs.figure, "comparison_" * info.experiment.basics.name * "_" * info.experiment.basics.domain)
 
-    foreach(costOpt) do var_row
-        v = var_row.variable
-        println("plot obs::", v)
-        v = (var_row.mod_field, var_row.mod_subfield)
-        vinfo = getVariableInfo(v, info.experiment.basics.temporal_resolution)
-        v = vinfo["standard_name"]
-        lossMetric = var_row.cost_metric
-        loss_name = nameof(typeof(lossMetric))
-        if loss_name in (:NNSEInv, :NSEInv)
-            lossMetric = NSE()
-        end
-        (obs_var, obs_σ, def_var) = getData(default_data, observation, var_row)
-        (_, _, opt_var) = getData(optimized_data, observation, var_row)
-        obs_var_TMP = obs_var[:, 1, 1, 1]
-        non_nan_index = findall(x -> !isnan(x), obs_var_TMP)
-        if length(non_nan_index) < 2
-            tspan = 1:length(obs_var_TMP)
-        else
-            tspan = first(non_nan_index):last(non_nan_index)
-        end
-        obs_σ = obs_σ[tspan]
-        obs_var = obs_var[tspan, 1, 1, 1]
-        def_var = def_var[tspan, 1, 1, 1]
-        opt_var = opt_var[tspan, 1, 1, 1]
-    
-        xdata = [info.helpers.dates.range[tspan]...]
-        obs_var_n, obs_σ_n, def_var_n = getDataWithoutNaN(obs_var, obs_σ, def_var)
-        obs_var_n, obs_σ_n, opt_var_n = getDataWithoutNaN(obs_var, obs_σ, opt_var)
-        metr_def = metric(lossMetric, def_var_n, obs_var_n, obs_σ_n)
-        metr_opt = metric(lossMetric, opt_var_n, obs_var_n, obs_σ_n)
-        plot(xdata, obs_var; label="obs", seriestype=:scatter, mc=:black, ms=4, lw=0, ma=0.65, left_margin=1plots_cm)
-        plot!(xdata, def_var, color=:steelblue2, lw=1.5, ls=:dash, left_margin=1plots_cm, legend=:outerbottom, legendcolumns=3, label="def ($(round(metr_def, digits=2)))", size=(2000, 1000), title="$(vinfo["long_name"]) ($(vinfo["units"])) -> $(nameof(typeof(lossMetric)))")
-        plot!(xdata, opt_var; color=:seagreen3, label="opt ($(round(metr_opt, digits=2)))", lw=1.5, ls=:dash)
-        ylabel!("$(vinfo["standard_name"])")
-        savefig(fig_prefix * "_$(v).png")
-    end
+    # debug plots for the same optimized/default data (see below) - flagging a behavior
+    # change vs. the original inline code: plotTimeSeries's shared helper reduces the
+    # pixel dimension via `mean(...; dims=3)`, whereas this script previously indexed a
+    # single site directly (`[:, :, 1, 1]`); for a single-site run these are equivalent.
+    plotTimeSeries(info, optimized_data, default_data)
 
     # save the outcubes
-
 
     optimized_data = values(optimized_data)
     default_data = values(default_data)
@@ -132,31 +100,5 @@ for domain ∈ sites
 
     saveOutCubes(info.output.file_info.file_prefix, info.output.file_info.global_metadata, optimized_data, output_dims, output_vars, "nc", info.experiment.basics.temporal_resolution, DoSaveSingleFile())
     saveOutCubes(info.output.file_info.file_prefix, info.output.file_info.global_metadata, optimized_data, output_dims, output_vars, "nc", info.experiment.basics.temporal_resolution, DoNotSaveSingleFile())
-
-
-    # plot the debug figures
-    default(titlefont=(20, "times"), legendfontsize=18, tickfont=(15, :blue))
-    fig_prefix = joinpath(info.output.dirs.figure, "debug_" * info.experiment.basics.name * "_" * info.experiment.basics.domain)
-    for (o, v) in enumerate(output_vars)
-        def_var = default_data[o][:, :, 1, 1]
-        opt_var = optimized_data[o][:, :, 1, 1]
-        vinfo = getVariableInfo(v, info.experiment.basics.temporal_resolution)
-        v = vinfo["standard_name"]
-        println("plot debug::", v)
-        xdata = [info.helpers.dates.range...]
-        if size(opt_var, 2) == 1
-            plot(xdata, def_var[:, 1]; label="def ($(round(SindbadTEM.mean(def_var[:, 1]), digits=2)))", size=(2000, 1000), title="$(vinfo["long_name"]) ($(vinfo["units"]))", left_margin=1plots_cm)
-            plot!(xdata, opt_var, color=:seagreen3[:, 1]; label="opt ($(round(SindbadTEM.mean(opt_var[:, 1]), digits=2)))")
-            ylabel!("$(vinfo["standard_name"])", font=(20, :green))
-            savefig(fig_prefix * "_$(v).png")
-        else
-            foreach(axes(opt_var, 2)) do ll
-                plot(xdata, def_var[:, ll]; label="def ($(round(SindbadTEM.mean(def_var[:, ll]), digits=2)))", size=(2000, 1000), title="$(vinfo["long_name"]), layer $(ll),  ($(vinfo["units"]))", left_margin=1plots_cm)
-                plot!(xdata, opt_var[:, ll]; color=:seagreen3, label="opt ($(round(SindbadTEM.mean(opt_var[:, ll]), digits=2)))")
-                ylabel!("$(vinfo["standard_name"])", font=(20, :green))
-                savefig(fig_prefix * "_$(v)_$(ll).png")
-            end
-        end
-    end
 
 end
--- examples/exp_WROASTED/test_WROASTED.jl
+++ examples/exp_WROASTED/test_WROASTED.jl
@@ -1,6 +1,7 @@
 using Revise
 using Sindbad
-using CMAEvolutionStrategy
+using Plots
+# using CMAEvolutionStrategy
 # Simulation
 # using CairoMakie
 
@@ -50,47 +51,5 @@ run_helpers = prepTEM(forcing, info);
 @time output_cost = runExperimentCost(experiment_json; replace_info=replace_info);
 @time out_opti = runExperimentOpti(experiment_json; replace_info=replace_info);
 
-observation = out_opti.observation
-
 # some plots
-def_dat = out_opti.output.default;
-opt_dat = out_opti.output.optimized;
-costOpt = prepCostOptions(observation, info.optimization.cost_options);
-plots_default(titlefont=(20, "times"), legendfontsize=18, tickfont=(15, :blue))
-foreach(costOpt) do var_row
-    v = var_row.variable
-    println("plot obs::", v)
-    v = (var_row.mod_field, var_row.mod_subfield)
-    vinfo = getVariableInfo(v, info.experiment.basics.temporal_resolution)
-    v = vinfo["standard_name"]
-    lossMetric = var_row.cost_metric
-    loss_name = nameof(typeof(lossMetric))
-    if loss_name in (:NNSEInv, :NSEInv)
-        lossMetric = NSE()
-    # else
-        # lossMetric = Pcor()    
-    end
-    (obs_var, obs_σ, def_var) = getData(def_dat, observation, var_row)
-    (_, _, opt_var) = getData(opt_dat, observation, var_row)
-    obs_var_TMP = obs_var[:, 1, 1, 1]
-    non_nan_index = findall(x -> !isnan(x), obs_var_TMP)
-    if length(non_nan_index) < 2
-        tspan = 1:length(obs_var_TMP)
-    else
-        tspan = first(non_nan_index):last(non_nan_index)
-    end
-    obs_σ = obs_σ[tspan]
-    obs_var = obs_var[tspan, 1, 1, 1]
-    def_var = def_var[tspan, 1, 1, 1]
-    opt_var = opt_var[tspan, 1, 1, 1]
-
-    xdata = [info.helpers.dates.range[tspan]...]
-    obs_var_n, obs_σ_n, def_var_n = getDataWithoutNaN(obs_var, obs_σ, def_var)
-    obs_var_n, obs_σ_n, opt_var_n = getDataWithoutNaN(obs_var, obs_σ, opt_var)
-    metr_def = metric(lossMetric, def_var_n, obs_var_n, obs_σ_n)
-    metr_opt = metric(lossMetric, opt_var_n, obs_var_n, obs_σ_n)
-    plots_plot(xdata, obs_var; label="obs", seriestype=:scatter, mc=:black, ms=4, lw=0, ma=0.65, left_margin=1plots_cm)
-    plots_plot!(xdata, def_var, color=:steelblue2, lw=1.5, ls=:dash, left_margin=1plots_cm, legend=:outerbottom, legendcolumns=3, label="def ($(round(metr_def, digits=2)))", size=(2000, 1000), title="$(vinfo["long_name"]) ($(vinfo["units"])) -> $(nameof(typeof(lossMetric)))")
-    plots_plot!(xdata, opt_var; color=:seagreen3, label="opt ($(round(metr_opt, digits=2)))", lw=1.5, ls=:dash)
-    plots_savefig(joinpath(info.output.dirs.figure, "wroasted_$(domain)_$(v).png"))
-end
+plotTimeSeries(out_opti)
