911fe3ec4ba8d8a1f1e9de06784e4dd4d570721a
dr-ko <skoirala@bgc-jena.mpg.de>
2026-08-02 04:02:23 +0200

    Archive legacy SindbadTEM per-approach tests, no longer run

--- SindbadTEM/test/_archive/utils_test.jl
+++ SindbadTEM/test/_archive/utils_test.jl
@@ -7,8 +7,8 @@ include("./mock_input/helpers.jl")
 
 @testset "utilsCore: @rep_elem" begin
     helpers = deepcopy(tmp_helpers) # we need a better way, other than enforcing `helpers` as a global variable.
-    land = deepcopy(tmp_land)  # copy our tmp_land, we will modify field within.
-    @unpack_nt cSoilSlow  ⇐ land.pools # type \Leftarrow <TAB> (REPL) or (Enter to suggestion in editor)
+    land_copy = deepcopy(land)  # copy our land test data, we will modify field within.
+    @unpack_nt cSoilSlow  ⇐ land_copy.pools # type \Leftarrow <TAB> (REPL) or (Enter to suggestion in editor)
     @rep_elem 100 ⇒ (cSoilSlow, 1, :cSoilSlow) # replace (whole bucket) without mutation and without allocations!
     # type \Leftarrow <TAB> (REPL) or (Enter to suggestion in editor)
     @test cSoilSlow[1] == 100.0
