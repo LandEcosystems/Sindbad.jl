e23b99ab92e92419e81612bd415338a7ca5f68e6
dr-ko <skoirala@bgc-jena.mpg.de>
2026-09-22 03:29:35 +0200

    fix errors in cFire

--- SindbadTEM/src/Processes/cFire/cFire_simple.jl
+++ SindbadTEM/src/Processes/cFire/cFire_simple.jl
@@ -28,39 +28,42 @@ function compute(params::cFire_simple, forcing, land, helpers)
         (c_Fire_cci, c_Fire_k) ⇐ land.diagnostics
         cEco ⇐ land.pools
         c_remain ⇐ land.states
-        (c_giver, c_taker, zix_cHeterotrophic) ⇐ land.cCycleBase
+        (c_flow_order, c_giver, c_taker, zix_cHeterotrophic) ⇐ land.cCycleBase
         c_model ⇐ land.models
+        z_zero ⇐ land.constants
     end
     # set c_fire_efflux and c_fire_mortality and cFireTotal to 0
-    cFireTotal = zero(eltype(cEco))
+    cFireTotal = z_zero
     @rep_vec c_fire_efflux ⇒ helpers.pools.zeros.cEco
     @rep_vec c_fire_mortality ⇒ helpers.pools.zeros.cEco
     @rep_vec c_fire_flow ⇒ helpers.pools.zeros.cEco
     zix_cVeg = helpers.pools.zix.cVeg
 
     # if there is not fire and no dieoff, pack and return
-    if c_fire_fba != zero(c_fire_fba) || c_f_cVeg_dieOff != zero(c_f_cVeg_dieOff)
+    if c_fire_fba != z_zero || c_f_cVeg_dieOff != z_zero
         for fO ∈ c_flow_order
-            c_giver[fO] ∈ zix_cVeg || continue
+            giver = c_giver[fO]
+            giver ∈ zix_cVeg || continue
+            taker = c_taker[fO]
             # total mortality fraction of vegetation pool
-            f_loss = c_f_cVeg_dieOff + c_fire_fba * c_Fire_k[zixVeg]
-            cLoss = (at_least_zero(cEco[zixVeg] - c_remain) * f_loss) * c_flow_QP_vec[fO]
+            f_loss = c_f_cVeg_dieOff + c_fire_fba * c_Fire_k[giver]
+            cLoss = (at_least_zero(cEco[giver] - c_remain) * f_loss) * c_flow_QP_vec[fO]
             # part that is combusted and that goes to the litter pools
-            cLossFire = safe_divide(cLoss * (c_fire_fba * c_Fire_k[zixVeg]), f_loss * c_Fire_cci[zixVeg])
+            cLossFire = safe_divide(cLoss * (c_fire_fba * c_Fire_k[giver]), f_loss * c_Fire_cci[giver])
             cLossNonFire = cLoss - cLossFire
 
-            @add_to_elem cLossFire ⇒ (c_fire_efflux, c_giver[fO])
-            @add_to_elem cLossNonFire ⇒ (c_fire_flow, taker[fO])
-            @add_to_elem cLoss ⇒ (c_fire_mortality, c_giver[fO])
+            @add_to_elem cLossFire ⇒ (c_fire_efflux, giver)
+            @add_to_elem cLossNonFire ⇒ (c_fire_flow, taker)
+            @add_to_elem cLoss ⇒ (c_fire_mortality, giver)
 
             #=
             # deplete pool
-            @add_to_elem -cLoss ⇒ (cEco, c_giver[fO])
+            @add_to_elem -cLoss ⇒ (cEco, giver)
             # transfer non combusted part
-            @add_to_elem cLossNonFire ⇒ (cEco, c_taker[fO])
+            @add_to_elem cLossNonFire ⇒ (cEco, taker)
             # feed c_fire_efflux and c_fire_mortality (@rep_elem)
-            @add_to_elem cLossFire ⇒ (c_fire_efflux, c_giver[fO])
-            @add_to_elem cLoss ⇒ (c_fire_mortality, c_giver[fO])
+            @add_to_elem cLossFire ⇒ (c_fire_efflux, giver)
+            @add_to_elem cLoss ⇒ (c_fire_mortality, giver)
             =#
         end
 
